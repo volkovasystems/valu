f067f410044fcb95bc1ed19c9a44ba236c9edb93
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-26 19:33:19 +0800

    Updated test.

--- valu-test.js
+++ valu-test.js
@@ -2,6 +2,6 @@
 const assert = require( "assert" );
 const valu = require( "./valu.js" );
 
-assert.deepEqual( valu( { "hello": "world" } ), [ "world" ], "should return array containing 'world'" );
+assert.deepEqual( valu( { "hello": "world" } ), [ "world" ], "should return [ 'world' ]" );
 
 console.log( "ok" );
