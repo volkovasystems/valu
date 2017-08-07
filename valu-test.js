8545bb5dd0fa605095d9a2f760083231971e84db
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-07 17:16:55 +0800

    Updated dependency.

--- valu-test.js
+++ valu-test.js
@@ -2,6 +2,6 @@
 const assert = require( "assert" );
 const valu = require( "./valu.js" );
 
-assert.deepEqual( valu( { "hello": "world" } ), [ "world" ], "should return [ 'world' ]" );
+assert.deepEqual( valu( { "hello": "world" } ), [ "world" ], "should be equal to [ 'world' ]" );
 
 console.log( "ok" );
